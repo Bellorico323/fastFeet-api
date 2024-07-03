47d557d0a2a8c577254f1ca71893b22273653fc4
Bellorico323 <mabo.orico@gmail.com>
2024-07-02 21:11:21 -0300

    .

--- src/domain/application/repositories/deliveryman-repository.ts
+++ src/domain/application/repositories/deliveryman-repository.ts
@@ -4,4 +4,5 @@ export interface DeliverymanRepository {
   findByCpf(cpf: string): Promise<Deliveryman | null>
   register(Deliveryman: Deliveryman): Promise<void>
   save(Deliveryman: Deliveryman): Promise<void>
+  delete(Deliveryman: Deliveryman): Promise<void>
 }
