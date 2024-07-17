0b446a10744d6bdf714b9d31f3dbfcfc8e7021de
Bellorico323 <mabo.orico@gmail.com>
2024-07-17 13:47:18 -0300

    implement admin prisma repository

---
+++ src/infra/database/prisma/repositories/prisma-admins-repository.ts
@@ -0,0 +1,32 @@
+import { AdminRepository } from '@/domain/delivery/application/repositories/admin-repository'
+import { Admin } from '@/domain/delivery/enterprise/entities/admin'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaAdminMapper } from '../mappers/prisma-admins-mapper'
+
+@Injectable()
+export class PrismaAdminsRepository implements AdminRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<Admin | null> {
+    const admin = await this.prisma.admin.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!admin) {
+      return null
+    }
+
+    return PrismaAdminMapper.toDomain(admin)
+  }
+
+  async register(admin: Admin): Promise<void> {
+    const data = PrismaAdminMapper.toPrisma(admin)
+
+    await this.prisma.admin.create({
+      data,
+    })
+  }
+}
