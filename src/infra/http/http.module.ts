e72b140881a0428cf239aaf1b113dcca92d4cb92
Bellorico323 <mabo.orico@gmail.com>
2024-07-16 22:38:22 -0300

    feat: Database module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateRecipientController } from './controllers/create-recipient.controller'
-import { PrismaService } from '../database/prisma/prisma.service'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateRecipientController,
   ],
-  providers: [PrismaService],
+  providers: [],
 })
 export class HttpModule {}
